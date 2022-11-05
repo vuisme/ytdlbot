3e981db10c14f34c4f08b875b24087c4f7e443d8
dlcvietnam <96235876+dlcvietnam@users.noreply.github.com>
2022-11-05 11:16:18 +0700

    Update tasks.py

--- ytdlbot/tasks.py
+++ ytdlbot/tasks.py
@@ -260,6 +260,19 @@ def ytdl_normal_download(bot_msg, client, url):
         video_paths = result["filepath"]
         bot_msg.edit_text('Download complete. Sending now...')
         logging.info(result)
+        for video_path in video_paths:
+            # normally there's only one video in that path...
+            extPath = pathlib.Path(video_path).suffix
+            st_size = os.stat(video_path).st_size
+            if (extPath == '.mp4' or extPath == '.mkv'):
+                if st_size > TG_MAX_SIZE:
+                    t = f"Your video({sizeof_fmt(st_size)}) is too large for Telegram. I'll upload it to transfer.sh"
+                    bot_msg.edit_text(t)
+                    client.send_chat_action(chat_id, 'upload_document')
+                    client.send_message(chat_id, upload_transfer_sh(bot_msg, video_paths))
+                    return
+                upload_processor(client, bot_msg, url, video_path)
+                bot_msg.edit_text('Download success!✅')
         lstimg = []
         for url_path in video_paths:
             # normally there's only one video in that path...
@@ -278,20 +291,7 @@ def ytdl_normal_download(bot_msg, client, url):
                 disable_notification=True,
                 media=list(array)
             )
-            time.sleep(30)
-        for video_path in video_paths:
-            # normally there's only one video in that path...
-            extPath = pathlib.Path(video_path).suffix
-            st_size = os.stat(video_path).st_size
-            if extPath == '.mp4':
-                if st_size > TG_MAX_SIZE:
-                    t = f"Your video({sizeof_fmt(st_size)}) is too large for Telegram. I'll upload it to transfer.sh"
-                    bot_msg.edit_text(t)
-                    client.send_chat_action(chat_id, 'upload_document')
-                    client.send_message(chat_id, upload_transfer_sh(bot_msg, video_paths))
-                    return
-                upload_processor(client, bot_msg, url, video_path)
-                bot_msg.edit_text('Download success!✅')
+            time.sleep(60)
              
     else:
         client.send_chat_action(chat_id, 'typing')
