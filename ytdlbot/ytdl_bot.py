4259f8f445bcaa0ba760e2619c8e021a6834494c
dlcvietnam <96235876+dlcvietnam@users.noreply.github.com>
2022-11-06 19:49:56 +0700

    Update ytdl_bot.py

--- ytdlbot/ytdl_bot.py
+++ ytdlbot/ytdl_bot.py
@@ -23,7 +23,10 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from tgbot_ping import get_runtime
 from youtubesearchpython import VideosSearch
-
+from urllib.parse import parse_qs, urlparse, unquote
+from os.path import splitext, basename
+from bs4 import BeautifulSoup
+from urllib.request import urlopen, Request
 from client_init import create_app
 from config import (AUTHORIZED_USER, ENABLE_CELERY, ENABLE_VIP, OWNER,
                     REQUIRED_MEMBERSHIP)
@@ -258,6 +261,46 @@ def vip_handler(client: "Client", message: "types.Message"):
 
 @app.on_message(filters.incoming & filters.text)
 @private_use
+def tbcn(tbcnurl):
+    #logger.info(tbcnurl)
+    uagent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92 ChannelId(3) Nebula PSDType(1) AlipayDefined(nt:WIFI,ws:375|647|2.0) AliApp(AP/10.0.1.123008) AlipayClient/10.0.1.123008 Alipay Language/zh-Hans"
+    headers = {'User-Agent': uagent}
+    openpage = urlopen(Request(tbcnurl, headers=headers))
+    #print(openpage)
+    search_page_soup = BeautifulSoup(openpage, 'html5lib')
+    #print(search_page_soup)
+    #logger.info(search_page_soup)
+    head = search_page_soup.find_all('head')
+    #logger.info(head)
+    pattern = re.compile(r"var url = '(.*?)';$", re.MULTILINE | re.DOTALL)
+    #logger.info(head.text)
+    #logger.info(pattern.search(head[0].text))
+    #logger.info(pattern.search(head[0].text).group(1))
+    tblink = pattern.search(head[0].text).group(1)
+    #logger.info(tblink)
+    return tblink
+    
+def qr1688(url1688):
+    #logger.info(tbcnurl)
+    uagent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92 ChannelId(3) Nebula PSDType(1) AlipayDefined(nt:WIFI,ws:375|647|2.0) AliApp(AP/10.0.1.123008) AlipayClient/10.0.1.123008 Alipay Language/zh-Hans"
+    headers = {'User-Agent': uagent}
+    openpage = urlopen(Request(url1688, headers=headers))
+    #print(openpage)
+    search_page_soup = BeautifulSoup(openpage, 'html.parser')
+    #print(search_page_soup)
+    #logger.info(search_page_soup)
+    script = search_page_soup.find_all('script')
+    #print(script[1])
+    #logger.info(head)
+    pattern = re.compile(r"var shareUrl = '(.*?)';$", re.MULTILINE | re.DOTALL)
+    #logger.info(head.text)
+    #logger.info(pattern.search(head[0].text))
+    #logger.info(pattern.search(head[0].text).group(1))
+    alibabalink = pattern.search(script[1].text).group(1)
+    #print(alibabalink)
+    #logger.info(tblink)
+ 
+    return alibabalink
 def download_handler(client: "Client", message: "types.Message"):
     # check remaining quota
     red = Redis()
@@ -265,10 +308,44 @@ def download_handler(client: "Client", message: "types.Message"):
     client.send_chat_action(chat_id, 'typing')
     red.user_count(chat_id)
 
-    url = re.sub(r'/ytdl\s*', '', message.text)
-    #url = re.search("(?P<linkrm>https?://[^\s]+)", message.text).group("linkrm")
+    #url = re.sub(r'/ytdl\s*', '', message.text)
+    url = re.search("(?P<linkrm>https?://[^\s]+)", message.text).group("linkrm")
     logging.info("start %s", url)
-
+    if "item.taobao.com" in url:
+      vid = parse_qs(urlparse(url).query).get('id')
+      url = "https://world.taobao.com/item/" + str(vid[0]) + ".htm"
+    if "offerId" in url:
+      vid = parse_qs(urlparse(url).query).get('offerId')
+      url = "https://m.1688.com/offer/" + str(vid[0]) + ".html"
+    if "intl.taobao.com" in url:
+      vid = parse_qs(urlparse(url).query).get('id')
+      url = "https://world.taobao.com/item/" + str(vid[0]) + ".htm"
+    if "tmall.com" in url:
+      vid = parse_qs(urlparse(url).query).get('id')
+      url = "https://item.taobao.com/item.htm?id=" + str(vid[0])
+    if "1688.com/offer/" in url:
+      vid = os.path.basename(urlparse(url).path)
+      url = "https://m.1688.com/offer/" + vid
+      logger.info("link sau khi convert")
+      logger.info(url)
+    if "qr.1688.com" in url:
+      oklink = qr1688(url)
+      logger.info("link 1688 sau khi convert")
+      logger.info(url)
+      url = unquote(unquote(oklink))
+    if "tb.cn" in url:
+      linktb = tbcn(url)
+      vid = parse_qs(urlparse(linktb).query).get('id')
+      if "a.m.taobao.com" in linktb:
+        disassembled = urlparse(linktb)
+        videoid, file_ext = splitext(basename(disassembled.path))
+        videoid = re.sub(r"\D", "", videoid)
+      else:
+          videoid = str(vid[0])
+      url = "https://world.taobao.com/item/" + videoid + ".htm"
+      logger.info("tb.cn convert xong")
+      logger.info(linktb)
+      logger.info(url)
     if not re.findall(r"^https?://", url.lower()):
         red.update_metrics("bad_request")
         message.reply_text("I think you should send me a link.", quote=True)
