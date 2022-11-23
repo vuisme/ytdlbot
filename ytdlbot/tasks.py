7178ebf48ec9c53be150e3d8af39aa7c666bd136
dlcvietnam <96235876+dlcvietnam@users.noreply.github.com>
2022-11-23 11:30:55 +0700

    Update tasks.py

--- ytdlbot/tasks.py
+++ ytdlbot/tasks.py
@@ -278,12 +278,13 @@ def ytdl_normal_download(bot_msg, client, url):
             st_size = os.stat(video_path).st_size
             if (extPath == '.mp4' or extPath == '.mkv' or extPath == '.webm' or extPath == '.mov'):
                 if st_size > TG_MAX_SIZE:
-                    bot_msg.edit_text(f"Your video({sizeof_fmt(st_size)}) is too large for Telegram.")
-                    # client.send_chat_action(chat_id, 'upload_document')
-                    # client.send_message(chat_id, upload_transfer_sh(bot_msg, video_paths))
-                    continue
+                    t = f"Your video({sizeof_fmt(st_size)}) is too large for Telegram. I'll upload it to transfer.sh"
+                    bot_msg.edit_text(t)
+                    client.send_chat_action(chat_id, 'upload_document')
+                    client.send_message(chat_id, upload_transfer_sh(bot_msg, video_paths))
+                    return
                 upload_processor(client, bot_msg, url, video_path)
-            bot_msg.edit_text('Download Video Success!✅')
+        bot_msg.edit_text('Download Video Success!✅')
     else:
         client.send_chat_action(chat_id, 'typing')
         tb = result["error"][0:4000]
