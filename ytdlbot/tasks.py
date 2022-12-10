8b0c8036fc606e2f5110c42fbbeff611015e784c
dlcvietnam <dlc.nguyenvutran@gmail.com>
2022-12-10 11:51:29 +0700

    quere route

--- ytdlbot/tasks.py
+++ ytdlbot/tasks.py
@@ -535,7 +535,21 @@ def async_task(task_name, *args):
             task_name.apply_async(args=args, queue=destination_taobao)
             return
         else:
-            task_name.delay(*args)
+            t0 = time.time()
+            inspect = app.control.inspect()
+            worker_stats = inspect.stats()
+            route_queues = []
+            # padding = math.ceil(sum([i['pool']['max-concurrency'] for i in worker_stats.values()]) / len(worker_stats))
+            for worker_name, stats in worker_stats.items():
+                route = worker_name.split('@')[1]
+                logging.info(route)
+                concurrency = stats['pool']['max-concurrency']
+                logging.info(concurrency)
+                route_queues.extend([route])
+            logging.info("route_queue is %s", route_queues)
+            destination_taobao = random.choice(route_queues)
+            logging.info("Selecting worker %s from %s in %.2fs", destination_taobao, route_queues, time.time() - t0)
+            task_name.apply_async(args=args, queue=destination_taobao)
             return
 
     t0 = time.time()
