50e9cd29f1a7142605726c1774d2b1f87969224c
cpanel10x <96235876+dlcvietnam@users.noreply.github.com>
2022-12-27 11:28:33 +0700

    merge

--- ytdlbot/ytdl_bot.py
+++ ytdlbot/ytdl_bot.py
@@ -361,8 +361,8 @@ def download_handler(client: "Client", message: "types.Message"):
         logging.info(linktb)
         logging.info(url)
     logging.info("start get %s", url)
-    if re.findall(r"^https://www\.youtube\.com/channel/", VIP.extract_canonical_link(url)):
-        message.reply_text("Channel download is disabled now. Please send me individual video link.", quote=True)
+    if re.findall(r"^https://www\.youtube\.com/channel/", VIP.extract_canonical_link(url)) or "list" in url:
+        message.reply_text("Channel/list download is disabled now. Please send me individual video link.", quote=True)
         red.update_metrics("reject_channel")
         return
     # non vip user, consume too many token
