8da6d3ecb545152322f0c18f8ca67d17699c4bb6
cpanel10x <me@vutn.net>
2023-11-14 14:27:56 +0700

    Update low_id.sh

--- scripts/low_id.sh
+++ scripts/low_id.sh
@@ -5,7 +5,7 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/ga
 if docker-compose logs --tail=100 ytdl | grep -q "The msg_id is too low"; then
     # If the string is found, stop the ytdl service
     echo "ytdl service stopped due to 'The msg_id is too low' found in logs."
-    docker-compose stop ytdl && docker-compose rm ytdl && docker-compose up -d
+    restart
 
 else
     echo "String not found in logs."
