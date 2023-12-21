2a6860c26c9dda8744629623b1d83d13a42c1b06
vuisme <me@vutn.net>
2023-12-21 22:55:18 +0700

    update image filter push

--- ytdlbot/tasks.py
+++ ytdlbot/tasks.py
@@ -495,7 +495,7 @@ def upload_processor(client: Client, bot_msg: types.Message, url: str, vp_or_fid
             logging.info(vp_or_fid)     
             res_msg = client.send_video(
                 chat_id,
-                'a.mp4',
+                '/a.mp4',
                 **meta,
             )
             logging.info(res_msg)
