69094989e614373a018f0561e65c4f3cb40ba3af
vuisme <me@vutn.net>
2023-12-21 22:16:09 +0700

    update image filter push

--- ytdlbot/tasks.py
+++ ytdlbot/tasks.py
@@ -341,19 +341,6 @@ def ytdl_normal_download(client: Client, bot_msg: types.Message | typing.Any, ur
     logging.info(mp4_paths)
     client.send_chat_action(chat_id, enums.ChatAction.UPLOAD_DOCUMENT)
     bot_msg.edit_text("Download complete. Sending now...")
-    min_size_kb = 20
-    image_lists = filter_images(lst_paths, min_size_kb)
-    if image_lists:
-        img_lists = []
-        max_images_per_list = 9
-        split_lists = split_image_lists(img_lists, max_images_per_list)
-        logging.info(split_lists)
-        for i, image_paths in enumerate(split_lists, start=1):
-            logging.info(image_paths)
-            # upload_processor(client, bot_msg, url, image_paths)
-    else:
-        logging.info("Không có ảnh")
-    
     try:
         upload_processor(client, bot_msg, url, mp4_paths)
     except pyrogram.errors.Flood as e:
