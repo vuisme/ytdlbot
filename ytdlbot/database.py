48669bde84b25a08c50f5fd4947d58490fe3cafa
vuisme <me@vutn.net>
2023-12-26 11:27:28 +0700

    bvuild

--- ytdlbot/database.py
+++ ytdlbot/database.py
@@ -259,7 +259,6 @@ class MySQL:
             self.con = pymysql.connect(
                 host=MYSQL_HOST, port=int(MYSQL_PORT), user=MYSQL_USER, password=MYSQL_PASS, database="ytdl", charset="utf8mb4"
             )
-            self.con.ping(reconnect = True)
         except Exception:
             logging.warning("MySQL connection failed, using fake mysql instead.")
             self.con = FakeMySQL()
