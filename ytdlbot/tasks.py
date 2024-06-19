25304ce3a736b9f1df4bcef3d31a6c31da04c1d0
cpanel10x <me@vutn.net>
2024-06-20 04:52:16 +0700

    Update tasks.py

--- ytdlbot/tasks.py
+++ ytdlbot/tasks.py
@@ -383,7 +383,7 @@ def spdl_normal_download(client: Client, bot_msg: types.Message | typing.Any, ur
             try:
                 logging.info("send lan %s", i)
                 logging.info(image_paths)
-                client.send_media_group(chat_id, generate_input_media2(image_paths,""))
+                client.send_media_group(chat_id, generate_input_media(image_paths,""))
             except pyrogram.errors.Flood as e:
                 logging.critical("FloodWait from Telegram: %s", e)
                 client.send_message(
@@ -392,7 +392,7 @@ def spdl_normal_download(client: Client, bot_msg: types.Message | typing.Any, ur
                 )
                 client.send_message(OWNER, f"CRITICAL INFO: {e}")
                 time.sleep(e.value)
-                client.send_media_group(chat_id, generate_input_media2(image_paths,""))
+                client.send_media_group(chat_id, generate_input_media(image_paths,""))
 
     else:
         logging.info("Không có ảnh")    
